9509c89f008b86d4aed2c425dc19e4984e3a9290
Johan van Zonneveld <johan@vzonneveld.nl>
2020-10-06 14:12:17 +0200

    Update cucumber_runner.rb

--- lib/knapsack/runners/cucumber_runner.rb
+++ lib/knapsack/runners/cucumber_runner.rb
@@ -6,10 +6,10 @@ module Knapsack
 
         Knapsack.logger.info
         Knapsack.logger.info "NODE #{ENV['TEST_ENV_NUMBER'] || 0}: Report features:".blue
-        Knapsack.logger.info allocator.report_node_tests.blue
+        Knapsack.logger.info allocator.report_node_tests.map { |f| f.blue }
         Knapsack.logger.info
         Knapsack.logger.info "NODE #{ENV['TEST_ENV_NUMBER'] || 0}: Leftover features:".blue
-        Knapsack.logger.info allocator.leftover_node_tests.blue
+        Knapsack.logger.info allocator.leftover_node_tests.map { |f| f.blue }
         Knapsack.logger.info
 
         cmd = %Q[bundle exec bin/cucumber #{args} --require #{allocator.test_dir} -- #{allocator.stringify_node_tests}]
