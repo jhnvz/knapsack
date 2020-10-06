b4175d634130aa60770b2c4116f75745c8a632c2
Johan van Zonneveld <johan@vzonneveld.nl>
2020-10-06 21:41:47 +0200

    Update rspec_runner.rb

--- lib/knapsack/runners/rspec_runner.rb
+++ lib/knapsack/runners/rspec_runner.rb
@@ -3,12 +3,14 @@ module Knapsack
     class RSpecRunner
       def self.run(args)
         allocator = Knapsack::AllocatorBuilder.new(Knapsack::Adapters::RSpecAdapter).allocator
-
+        
+        node_number = ENV['TEST_ENV_NUMBER']
+        
         Knapsack.logger.info
-        Knapsack.logger.info "NODE #{ENV['TEST_ENV_NUMBER'] || 0}: Report specs:".blue
+        Knapsack.logger.info "NODE #{node_number.blank ? 0 : node_number}: Report specs:".blue
         Knapsack.logger.info allocator.report_node_tests.map { |f| f.blue }
         Knapsack.logger.info
-        Knapsack.logger.info "NODE #{ENV['TEST_ENV_NUMBER'] || 0}: Leftover specs:".blue
+        Knapsack.logger.info "NODE #{node_number.blank ? 0 : node_number}: Leftover specs:".blue
         Knapsack.logger.info allocator.leftover_node_tests.map { |f| f.blue }
         Knapsack.logger.info
 
