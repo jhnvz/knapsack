c3b7d53f6752d0166b99760c6d539a47248ab1f0
Johan van Zonneveld <johan@vzonneveld.nl>
2020-10-06 13:59:58 +0200

    Add color

--- lib/knapsack/runners/cucumber_runner.rb
+++ lib/knapsack/runners/cucumber_runner.rb
@@ -5,11 +5,11 @@ module Knapsack
         allocator = Knapsack::AllocatorBuilder.new(Knapsack::Adapters::CucumberAdapter).allocator
 
         Knapsack.logger.info
-        Knapsack.logger.info "#{ENV['TEST_ENV_NUMBER']}: Report features:"
-        Knapsack.logger.info allocator.report_node_tests
+        Knapsack.logger.info "NODE #{ENV['TEST_ENV_NUMBER'] || 0}: Report features:".blue
+        Knapsack.logger.info allocator.report_node_tests.blue
         Knapsack.logger.info
-        Knapsack.logger.info "#{ENV['TEST_ENV_NUMBER']}: Leftover features:"
-        Knapsack.logger.info allocator.leftover_node_tests
+        Knapsack.logger.info "NODE #{ENV['TEST_ENV_NUMBER'] || 0}: Leftover features:".blue
+        Knapsack.logger.info allocator.leftover_node_tests.blue
         Knapsack.logger.info
 
         cmd = %Q[bundle exec bin/cucumber #{args} --require #{allocator.test_dir} -- #{allocator.stringify_node_tests}]
