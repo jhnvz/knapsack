e72a00116502d97f774c28c56a12f0f51ec16d1c
Johan van Zonneveld <johan@vzonneveld.nl>
2020-10-06 21:42:45 +0200

    Update cucumber_runner.rb

--- lib/knapsack/runners/cucumber_runner.rb
+++ lib/knapsack/runners/cucumber_runner.rb
@@ -3,12 +3,14 @@ module Knapsack
     class CucumberRunner
       def self.run(args)
         allocator = Knapsack::AllocatorBuilder.new(Knapsack::Adapters::CucumberAdapter).allocator
-
+        
+        node_number = ENV['TEST_ENV_NUMBER']       
+        
         Knapsack.logger.info
-        Knapsack.logger.info "NODE #{ENV['TEST_ENV_NUMBER'] || 0}: Report features:".blue
+        Knapsack.logger.info "NODE #{node_number.blank? ? 0 : node_number}: Report features:".blue
         Knapsack.logger.info allocator.report_node_tests.map { |f| f.blue }
         Knapsack.logger.info
-        Knapsack.logger.info "NODE #{ENV['TEST_ENV_NUMBER'] || 0}: Leftover features:".blue
+        Knapsack.logger.info "NODE #{node_number.blank? ? 0 : node_number}: Leftover features:".blue
         Knapsack.logger.info allocator.leftover_node_tests.map { |f| f.blue }
         Knapsack.logger.info
 
